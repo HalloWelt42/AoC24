f3f148a06a7648eac8f148ff101a122e7c323da7
Alpha <my@jobmagnetix.de>
2024-12-10 03:28:54 +0100

    xdebug mapping on phpstorm, welcome msg

--- app/src/Controller/AocController.php
+++ app/src/Controller/AocController.php
@@ -9,4 +9,11 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/aoc24')]
 class AocController extends AbstractController
 {
+    #[Route('/welcome', name: 'welcome', methods: ['GET'])]
+    public function index(): Response
+    {
+        return new Response('Welcome to Advent of Code 2024' . PHP_EOL);
+    }
+
+
 }
